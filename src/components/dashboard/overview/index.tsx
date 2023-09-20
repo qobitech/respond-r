ef390b3831257f32f0e8ee7c71556b8f10f47c77
Frank Qobi <qobitek@gmail.com>
2023-09-20 10:53:28 +0100

    feat: changed from localhost

--- src/components/dashboard/overview/index.tsx
+++ src/components/dashboard/overview/index.tsx
@@ -69,7 +69,7 @@ interface IUS {
 const useSocketIO = (): IUS => {
   const [hits, setHits] = useState<any>()
   const [feeds, setFeeds] = useState<any>()
-  const socket = io(`http://localhost:8000`)
+  const socket = io(`https://localhost:8000`)
 
   const sendRequest = (url: string) => {
     socket.emit("request_url", {
