4cc554f60d815a72299c10f7532517bc6a76a572
Frank Qobi <qobitek@gmail.com>
2023-09-21 05:42:39 +0100

    feat: updated socket url

--- src/components/dashboard/overview/index.tsx
+++ src/components/dashboard/overview/index.tsx
@@ -69,7 +69,7 @@ interface IUS {
 const useSocketIO = (): IUS => {
   const [hits, setHits] = useState<any>()
   const [feeds, setFeeds] = useState<any>()
-  const socket = io(`dev-respond-r.netlify.app`)
+  const socket = io(`dev-respond-r.netlify.app:8000`)
 
   const sendRequest = (url: string) => {
     socket.emit("request_url", {
