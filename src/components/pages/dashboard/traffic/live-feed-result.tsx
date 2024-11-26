8b99a526f583a4440f5d31712bbf30cb19338f6e
Frank Qobi <qobitek@gmail.com>
2024-11-26 12:21:59 +0100

    - removed demo data

--- src/components/pages/dashboard/traffic/live-feed-result.tsx
+++ src/components/pages/dashboard/traffic/live-feed-result.tsx
@@ -6,7 +6,7 @@ import { LiveHitItemComponent } from './live-hit-item-component'
 import { NoFeeds } from './no-feeds'
 import { useGlobalContext } from 'context/hooks'
 import { ICopyProps } from 'utils/hook'
-import { feedDemoDatas } from './mock-data'
+// import { feedDemoDatas } from './mock-data'
 
 export const LiveFeedResults = ({
   signalRProps,
@@ -25,8 +25,8 @@ export const LiveFeedResults = ({
 }) => {
   const { setSearch: setSearchValue } = useGlobalContext()
 
-  // const feeds = signalRProps.feeds
-  const feeds = feedDemoDatas
+  const feeds = signalRProps.feeds
+  // const feeds = feedDemoDatas
   return (
     <div className="live-feed-component-wrapper">
       {isFeed ? (
